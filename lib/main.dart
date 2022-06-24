3dce75c27457de3d95671dbe279eba0643d3d19b
nyto9999 <nyto9999@gmail.com>
2022-06-24 21:24:52 +0800

    playing around with basic widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('init testing'),
-    ));
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('flutter demo'),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Text(
+          'body',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'NotoSansTC',
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.redAccent,
+      ),
+    )));
