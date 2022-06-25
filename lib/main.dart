5625d2b4ac35ad4822c026105b132140d558da66
nyto9999 <nyto9999@gmail.com>
2022-06-25 14:29:45 +0800

    learning button, row, col

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-        home: Scaffold(
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('flutter demo'),
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Center(
-        child: Text(
-          'body',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'NotoSansTC',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('hellow world'),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.abc),
+            label: Text('icon'),
           ),
-        ),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(20.0),
+            child: Text('container'),
+          ),
+        ],
       ),
       drawer: Drawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Text('click'),
+        child: Text('d me'),
         backgroundColor: Colors.redAccent,
       ),
-    )));
+    );
+  }
+}
